c0bdb50a7d9b279a555193491276b10f411e6ad3
14joined <khangvutan@gmail.com>
2022-01-09 13:01:00 +0700

    add laptop prices model

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,37 +1,57 @@
-import random
 from flask import Flask, render_template, request
-from tensorflow.keras.datasets import boston_housing
-import house_prices
-    
-(train_data, train_targets), (test_data, test_targets) = (
-    boston_housing.load_data()
-)
+from laptop_prices import LaptopPrices
 
-mean = train_data.mean(axis=0)
-train_data -= mean
-std = train_data.std(axis=0)
-train_data /= std
-test_data -= mean
-test_data /= std
-
-model = house_prices.load_model(test_data.shape)
-predictions = model.predict(test_data)
+model = LaptopPrices()
 
 
 app = Flask(__name__)
 
+RAM = [2, 4, 8, 16]
+
+V_RAM = [0.0, 4.0, 8.0]
+
+DISPLAY = [13.3, 14, 15.6]
+
+RESOLUTION = [
+    "1920x1080",
+    "2560x1600"
+]
+
+SSD = [
+    256, 512, 1024
+]
+
+HDD = [
+    0, 512, 1024
+]
+
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html",
+                           RAM=RAM, V_RAM=V_RAM, DISPLAY=DISPLAY, RESOLUTION=RESOLUTION, SSD=SSD, HDD=HDD)
 
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    app.logger.warning(request.form.get("id"))
-    id = request.form.get("id", "undefined")
+    app.logger.warning(request.form.get("ram"))
+    ram = request.form.get("ram", "undefined")
 
-    if id == "undefined" or id == "" or int(id) >= test_data.shape[0]:
-        return render_template("predictions.html", id=0, price=predictions[0])
-    
-    return render_template("predictions.html", id=id, price=predictions[int(id)])
+    app.logger.warning(request.form.get("v_ram"))
+    v_ram = request.form.get("v_ram", "undefined")
+
+    app.logger.warning(request.form.get("display"))
+    display = request.form.get("display", "undefined")
+
+    app.logger.warning(request.form.get("resolution"))
+    resolution = request.form.get("resolution", "undefined")
+
+    app.logger.warning(request.form.get("ssd"))
+    ssd = request.form.get("ssd", "undefined")
+
+    app.logger.warning(request.form.get("hdd"))
+    hdd = request.form.get("hdd", "undefined")
+
+    price = model.predict(ram, v_ram, display, resolution, ssd, hdd)
+    return render_template("predictions.html", msg="hello, world!", price=price)
+
